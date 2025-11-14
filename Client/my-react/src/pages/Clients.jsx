965d6b716e6fbc581ebc4953e0f513edab55a9ff
Чичконаков Андрей Николаевич <29373288@s.mfui.ru>
2025-11-14 17:01:45 +0300

    14.11

---
+++ Client/my-react/src/pages/Clients.jsx
@@ -0,0 +1,5 @@
+export const Clients = () =>{
+    return(<>
+    <h1>СТРАНИЦА КЛИЕНТОВ</h1>
+    </>)
+}
---
+++ Client/my-react/src/pages/Products.jsx
@@ -0,0 +1,5 @@
+export const Products = () =>{
+    return(<>
+    <h1>СТРАНИЦА ПРОДУКТОВ</h1>
+    </>)
+}
